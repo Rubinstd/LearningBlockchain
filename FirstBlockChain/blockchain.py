a5e27c9c4f3060bf5aebda4bfa8f1807f5025848
drubinst <daniel.rubinstein@arcelormittal.com>
2019-07-22 11:43:16 -0400

    Added mining and flask rest-api

--- FirstBlockChain/blockchain.py
+++ FirstBlockChain/blockchain.py
@@ -8,6 +8,9 @@
 from hashlib import sha256
 import json
 
+from flask import Flask, request
+import requests
+
 # Class for defining a single block in the chain.
 class Block:
     # Constructor.
@@ -72,5 +75,91 @@ class BlockChain:
         # Add the block to the chain.
         self.chain.append(block)
         return True
+    # Prove that a block is valid with a given hash (i.e. the hash has 2 leading zeros and also the hash actually is the computed hash of the block).
     def is_valid_proof(self, block, block_hash):
         return (block_hash.startswith('0' * BlockChain.difficulty) and block_hash == block.compute_hash())
+    
+    # Add a transaction to unconfirmed transactions.
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
+    
+    # Mine function.
+    def mine(self):
+        if not self.unconfirmed_transactions:
+            return False
+        
+        last_block = self.last_block
+
+        new_block = Block(index=last_block.index + 1,
+                          transactions = self.unconfirmed_transactions,
+                          timestamp = time.time(),
+                          previous_hash = last_block.hash)
+        
+        proof = self.proof_of_work(new_block)
+        self.add_block(new_block, proof)
+
+        self.unconfirmed_transactions = []
+
+        return new_block.index
+
+
+########################################
+# Building Flask REST-API.
+
+
+app = Flask(__name__)
+
+blockchain = BlockChain()
+
+@app.route('/new_transaction', methods=['POST'])
+def new_transaction():
+    # Get inputs
+    tx_data = request.get_json()
+    required_fields = ["author", "content"]
+
+    # Check to make sure inputs are valid.
+    for field in required_fields:
+        if not tx_data.get(field):
+            return "Invalid Transaction Data", 404
+
+    # Set timestamp.
+    tx_data["timestamp"] = time.time()
+
+    # Add to blockchain.
+    blockchain.add_new_transaction(tx_data)
+
+    return "Success", 201
+
+# Get chain data.
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_data = []
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+    return json.dumps({ "length" : len(chain_data),
+                        "chain" : chain_data})
+# Request mine.
+@app.route('/mine', methods=['GET'])
+def mine_unconfirmed_transactions():
+    result = blockchain.mine()
+
+    if not result:
+        return "No transaction to mine"
+    return "Block #{} is mined.".format(result)
+
+@app.route('/pending_tx')
+def get_pending_tx():
+    return json.dumps(blockchain.unconfirmed_transactions)
+
+app.run(debug = True, port = 8000)
+
+#peers = set()
+
+#@app.route('/add_nodes', methods=['POST'])
+#def register_new_peers():
+#    nodes = request.get_json()
+
+#    if not nodes:
+#        return "Invalid data", 400
+#    for node in nodes:
+#        peers.add(node)
